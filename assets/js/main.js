661cee0388c781c472ba8f3750e5f8337cc0d6dc
Febrianto <febrianto.bekasi@gmail.com>
2022-09-12 18:09:57 +0700

    finish project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,32 @@
 const cardElems = document.querySelectorAll(".articles .card");
 
-cardElems.forEach((cardElem, index) => {
-  setTimeout(() => {
-    cardElem.classList.add("fade");
-  }, index * 800);
-});
+const showArticleCard = () => {
+  cardElems.forEach((cardElem, index) => {
+    setTimeout(() => {
+      cardElem.classList.add("fade");
+    }, index * 800);
+  });
+};
 
-console.log(cardElems);
+document.addEventListener("scroll", () => {
+  const otherArticleElem = document.getElementById("other-article");
+
+  if (
+    window.innerWidth > 1315 &&
+    window.scrollY >
+      otherArticleElem.offsetTop - (otherArticleElem.offsetHeight + 340)
+  ) {
+    showArticleCard();
+  } else if (
+    window.innerWidth > 880 &&
+    window.scrollY >
+      otherArticleElem.offsetTop - (otherArticleElem.offsetHeight + 200)
+  ) {
+    showArticleCard();
+  } else if (
+    window.scrollY >
+    otherArticleElem.offsetTop - otherArticleElem.offsetHeight
+  ) {
+    showArticleCard();
+  }
+});
